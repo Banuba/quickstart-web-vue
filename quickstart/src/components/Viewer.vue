843500ece029b3ba973d2199d18342771483b257
Alexander Popelnyk <alexander.popelnyk@banuba.com>
2024-01-04 16:15:49 +0100

    init vue quickstart

---
+++ quickstart/src/components/Viewer.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+  import { onMounted } from "vue";
+
+  import { BANUBA_CLIENT_TOKEN } from "./BanubaClientToken"
+  import { Webcam, Player, Module, Effect, Dom } from "@banuba/webar";
+
+  import wasm from "@banuba/webar/BanubaSDK.wasm?url";
+  import simd from "@banuba/webar/BanubaSDK.simd.wasm?url";
+  import data from "@banuba/webar/BanubaSDK.data?url";
+
+  // Find more about available modules:
+  // https://docs.banuba.com/face-ar-sdk-v1/generated/typedoc/classes/Module.html
+  import FaceTracker from "@banuba/webar/face_tracker.zip?url";
+  
+  import GlassesEffect from "./glasses_Banuba.zip?url";
+
+  let webcam: Webcam;
+  let player: Promise<Player>;
+
+  onMounted(() => {
+    webcam = new Webcam();
+    player = Player.create({
+      clientToken: BANUBA_CLIENT_TOKEN,
+      /**
+       * By default BanubaSDK.js tries to loads BanubaSDK.wasm and BanubaSDK.data files relative to itself.
+       * Since the BanubaSDK.js will be bundled to something like `static/js/[name].[hash].js` during a build
+       * and the BanubaSDK.wasm and BanubaSDK.data files may not lay next to the bundle file
+       * we have to tell the BanubaSDK where it can find these BanubaSDK.wasm and BanubaSDK.data files.
+       * @see {@link https://docs.banuba.com/generated/typedoc/globals.html#sdkoptions} further information}
+       */
+      locateFile: {
+        "BanubaSDK.wasm": wasm,
+        "BanubaSDK.simd.wasm": simd,
+        "BanubaSDK.data": data,
+      },
+    })
+
+    player.then((player) => 
+      player.addModule(new Module(FaceTracker)).then(() => {
+        player.use(webcam);
+        player.applyEffect(new Effect(GlassesEffect));
+        Dom.render(player, "#webar");
+      })
+    )
+  })
+</script>
+
+<template>
+  <div id="webar"></div>
+</template>
+
+<style scoped>
+
+</style>
---
+++ quickstart/src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import Viewer from '../components/Viewer.vue'
+</script>
+
+<template>
+  <main>
+    <Viewer />
+  </main>
+</template>
